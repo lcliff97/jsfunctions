484c0dea370576f6a43752ec1040e230d3676933
lcliff97 <louiecliff97@outlook.com>
2025-09-08 15:53:35 +0100

    js functions

---
+++ app.js
@@ -0,0 +1,12 @@
+function sayHello() {
+  console.log("Hello world!");
+}
+
+function happyBirthday() {
+  console.log("Happy birthday to you!");
+  console.log("Happy birthday to you!");
+  console.log("Happy birthday dear you");
+  console.log("Happy birthday to you!");
+}
+
+happyBirthday();
